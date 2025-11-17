407d2be1d73b00749e36190808981d92aec06df3
BaltazarBigos Pacha <tomaszsidor293@gmail.com>
2025-11-17 09:52:39 +0000

    Rectungalalalala

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
 
 class Rectangle {
+    private:
 
+    double width;
+    double hight;
+
+    public:
+    Rectangle(double w, double h){
+        width = w;
+        height = h;
+    }
+    void setWidth(double w){
+        Width = w;
+    }
+
+    void setHeight(double h){
+        height = h;
+    }
+
+    double getWidth() const{
+        return width;
+    }
+
+    double getHeight() const{
+        return height;
+    }
+
+    double area() const {
+        return height * width;
+    }
+
+    double perimeter() const{
+        return height * 2 + width * 2;
+    }
+
+    void display() const{
+        cout << "Rectangle: " << width << "x" << height << endl; 
+        cout << area() << endl;
+        cout << perimeter() << endl;
+    }
 };
